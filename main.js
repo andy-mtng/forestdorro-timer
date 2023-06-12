963622859e567304ba700a4b050beea672d99589
Andy N <andynguyen9001@gmail.com>
2023-06-11 21:54:08 -0600

    Add 3D model to scene

--- main.js
+++ main.js
@@ -1,24 +1,34 @@
 import * as THREE from 'three';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 const renderer = new THREE.WebGLRenderer();
+const loader = new GLTFLoader();
+renderer.setClearColor(0xffffff);
 
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
-
 // Moves the camera closer to the viewer and farther from the cube. z is depth
 camera.position.z = 5;
+camera.position.y = 1;
+
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshBasicMaterial( { color: 0x023020 } );
+const cube = new THREE.Mesh( geometry, material );
+// scene.add( cube );
+
+loader.load( 'public/rubix-cube.glb', function ( gltf ) {
+	scene.add( gltf.scene );
+}, undefined, function ( error ) {
+	console.error( error );
+} );
 
 function animate() {
 	requestAnimationFrame( animate );
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+    cube.rotation.y += 0.005;
+
 	renderer.render( scene, camera );
 }
 
